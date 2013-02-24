ddebfd032152216b01565eed926c4ed4792dbf09
David McClure <david.mcclure@virginia.edu>
2013-02-24 16:30:20 -0500

    Styles tag headers.

--- tag.php
+++ tag.php
@@ -15,7 +15,7 @@ get_header(); ?>
 
 				<header class="page-header">
 					<h1 class="page-title"><?php
-						printf( __( '// %s', 'toolbox' ), '<span>' . single_tag_title( '', false ) . '</span>' );
+						printf( __( '#%s', 'toolbox' ), '<span>' . single_tag_title( '', false ) . '</span>' );
 					?></h1>
 
 					<?php
