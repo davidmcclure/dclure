8b0a4fc98defd90ca4b1d9f7bf17cd199d06da25
David McClure <davidwilliammcclure@gmail.com>
2014-08-10 20:26:21 -0700

    Gets rid of rtl CSS file.

--- full-width-page.php
+++ full-width-page.php
@@ -13,11 +13,8 @@ get_header(); ?>
 			<div id="content" role="main">
 
 				<?php while ( have_posts() ) : the_post(); ?>
-
 				<?php get_template_part( 'content', 'page' ); ?>
-
 				<?php comments_template( '', true ); ?>
-
 				<?php endwhile; // end of the loop. ?>
 
 			</div><!-- #content -->
