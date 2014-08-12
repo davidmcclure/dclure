930a818973be28710459f085af63df3f9e3c8ff8
David McClure <davidwilliammcclure@gmail.com>
2014-08-12 14:36:09 -0700

    Footer.

--- footer.php
+++ footer.php
@@ -11,11 +11,18 @@
 
 	</div><!-- #main -->
 
-	<footer id="colophon" role="contentinfo">
-		<div id="site-generator">
-			<?php do_action( 'toolbox_credits' ); ?>
-            <span>All content licensed under <a href="http://creativecommons.org/licenses/by/3.0/">CC-BY</a>.</span>
-		</div>
+    <hr />
+
+	<footer id="colophon" role="contentinfo" class="row">
+        <ul>
+            <li><a href="mailto:davidwilliammcclure@gmail.com"><i class="fa fa-send fa-fw"></i> david@dclure.org</a></li>
+            <li><a href="https://twitter.com/clured"><i class="fa fa-twitter fa-fw"></i> clured</a></li>
+            <li><a href="https://github.com/davidmcclure"><i class="fa fa-github fa-fw"></i> davidmcclure</a></li>
+        </ul>
+        <ul>
+            <li>Yes to dance beneath the diamond sky.</li>
+            <li>All content licensed under <a href="http://creativecommons.org/licenses/by/3.0/">CC-BY</a>.</li>
+        </ul>
 	</footer><!-- #colophon -->
 
 </div><!-- #page -->
