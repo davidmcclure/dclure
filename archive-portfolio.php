cf3a6cbd020aa5f775d88ad5635f8e313f18d0c8
David McClure <davidwilliammcclure@gmail.com>
2014-08-13 02:50:13 -0400

    Shells in a very basic portfolio archive template.

---
+++ archive-portfolio.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The template for displaying Archive pages.
+ *
+ * Used to display archive-type pages if nothing more specific matches a query.
+ * For example, puts together date-based pages if no date.php file exists.
+ *
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Toolbox
+ * @since Toolbox 0.1
+ */
+
+get_header(); ?>
+
+		<section id="primary">
+			<div id="content" role="main">
+
+			<?php if ( have_posts() ) : ?>
+
+				<?php rewind_posts(); ?>
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php
+						/* Include the Post-Format-specific template for the content.
+						 * If you want to overload this in a child theme then include a file
+						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+						 */
+						get_template_part( 'content', get_post_format() );
+					?>
+
+				<?php endwhile; ?>
+
+			<?php endif; ?>
+
+			</div><!-- #content -->
+		</section><!-- #primary -->
+
+<?php get_footer(); ?>
