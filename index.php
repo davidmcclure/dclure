63fc82e65c309007a42f42653db014c735f1f335
David McClure <davidwilliammcclure@gmail.com>
2014-08-25 14:32:48 -0400

    Cleans up index.php.

--- index.php
+++ index.php
@@ -14,35 +14,30 @@
 
 get_header(); ?>
 
-		<div id="primary">
-			<div id="content" role="main">
+<div id="primary">
+  <div id="content" role="main">
 
-			<?php if ( have_posts() ) : ?>
+  <?php if ( have_posts() ) : ?>
 
-				<?php while ( have_posts() ) : the_post(); ?>
-                    <?php get_template_part( 'content', 'archive' ); ?>
-				<?php endwhile; ?>
+    <?php while ( have_posts() ) : the_post(); ?>
+      <?php get_template_part( 'content', 'archive' ); ?>
+    <?php endwhile; ?>
 
-				<?php toolbox_content_nav( 'nav-below' ); ?>
+    <?php toolbox_content_nav( 'nav-below' ); ?>
 
-			<?php else : ?>
+  <?php else : ?>
 
-				<article id="post-0" class="post no-results not-found">
+    <article id="post-0" class="post no-results not-found">
 
-					<header class="entry-header">
-						<h1 class="entry-title"><?php _e( 'Nothing Found', 'toolbox' ); ?></h1>
-					</header><!-- .entry-header -->
+      <header class="entry-header">
+        <h1 class="entry-title"><?php _e( 'Nothing Found', 'toolbox' ); ?></h1>
+      </header><!-- .entry-header -->
 
-					<div class="entry-content">
-						<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'toolbox' ); ?></p>
-						<?php get_search_form(); ?>
-					</div><!-- .entry-content -->
+    </article><!-- #post-0 -->
 
-				</article><!-- #post-0 -->
+  <?php endif; ?>
 
-			<?php endif; ?>
-
-			</div><!-- #content -->
-		</div><!-- #primary -->
+  </div><!-- #content -->
+</div><!-- #primary -->
 
 <?php get_footer(); ?>
