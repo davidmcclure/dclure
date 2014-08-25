311c94b0f37808cf5f30f44f6c5688036a3444f2
David McClure <davidwilliammcclure@gmail.com>
2014-08-25 14:38:34 -0400

    Cleans up archive-portfolio.php.

--- archive-portfolio.php
+++ archive-portfolio.php
@@ -13,20 +13,20 @@
 
 get_header(); ?>
 
-		<section id="primary">
-			<div id="content" role="main" class="portfolio">
+<section id="primary">
+  <div id="content" role="main" class="portfolio">
 
-			<?php if ( have_posts() ) : ?>
+  <?php if (have_posts()) : ?>
 
-				<?php rewind_posts(); ?>
+    <?php rewind_posts(); ?>
 
-				<?php while ( have_posts() ) : the_post(); ?>
-					<?php get_template_part( 'content', 'portfolio' ); ?>
-				<?php endwhile; ?>
+    <?php while (have_posts()) : the_post(); ?>
+      <?php get_template_part('content', 'portfolio'); ?>
+    <?php endwhile; ?>
 
-			<?php endif; ?>
+  <?php endif; ?>
 
-			</div><!-- #content -->
-		</section><!-- #primary -->
+  </div><!-- #content -->
+</section><!-- #primary -->
 
 <?php get_footer(); ?>
