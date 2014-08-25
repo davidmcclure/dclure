621422fe38ba1adbf245862e5a02f973f466bd7f
David McClure <davidwilliammcclure@gmail.com>
2014-08-25 14:47:33 -0400

    Gets rid of unused category.php.

--- index.php
+++ index.php
@@ -1,15 +1,7 @@
 <?php
+
 /**
- * The main template file.
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
- *
- * @package Toolbox
- * @since Toolbox 0.1
+ * Homepage.
  */
 
 get_header(); ?>
@@ -17,20 +9,22 @@ get_header(); ?>
 <div id="primary">
   <div id="content" role="main">
 
-  <?php if ( have_posts() ) : ?>
+  <?php if (have_posts()) : ?>
 
-    <?php while ( have_posts() ) : the_post(); ?>
-      <?php get_template_part( 'content', 'archive' ); ?>
+    <?php while (have_posts()) : the_post(); ?>
+      <?php get_template_part('content', 'archive'); ?>
     <?php endwhile; ?>
 
-    <?php toolbox_content_nav( 'nav-below' ); ?>
+    <?php toolbox_content_nav('nav-below'); ?>
 
   <?php else : ?>
 
     <article id="post-0" class="post no-results not-found">
 
       <header class="entry-header">
-        <h1 class="entry-title"><?php _e( 'Nothing Found', 'toolbox' ); ?></h1>
+        <h1 class="entry-title">
+          <?php _e( 'Nothing Found', 'toolbox' ); ?>
+        </h1>
       </header><!-- .entry-header -->
 
     </article><!-- #post-0 -->
