e2917630feb7a61d4c6bce1804c8c5fc8d4abc59
David McClure <davidwilliammcclure@gmail.com>
2014-08-25 14:37:09 -0400

    Cleans up 404.php.

--- single.php
+++ single.php
@@ -8,23 +8,22 @@
 
 get_header(); ?>
 
-		<div id="primary">
-			<div id="content" role="main">
+<div id="primary">
+  <div id="content" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+  <?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'content', 'single' ); ?>
+    <?php get_template_part( 'content', 'single' ); ?>
 
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || '0' != get_comments_number() )
-						comments_template( '', true );
-				?>
+    <?php
+      if ( comments_open() || '0' != get_comments_number() ) {
+        comments_template( '', true );
+      }
+    ?>
 
-			<?php endwhile; // end of the loop. ?>
+  <?php endwhile; // end of the loop. ?>
 
-			</div><!-- #content -->
-		</div><!-- #primary -->
+  </div><!-- #content -->
+</div><!-- #primary -->
 
-<?php //get_sidebar(); ?>
 <?php get_footer(); ?>
