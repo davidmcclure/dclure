767b6ee7287466a6096cc14a848b8c9b93194615
David McClure <davidwilliammcclure@gmail.com>
2014-08-25 15:19:55 -0400

    Footer cleanup.

--- footer.php
+++ footer.php
@@ -1,28 +1,54 @@
 <?php
+
 /**
- * The template for displaying the footer.
- *
- * Contains the closing of the id=main div and all content after
- *
- * @package Toolbox
- * @since Toolbox 0.1
+ * Footer.
  */
+
 ?>
 
 	</div><!-- #main -->
 
-    <hr />
+  <hr />
 
 	<footer id="colophon" role="contentinfo" class="row">
-        <ul>
-            <li><a href="mailto:davidwilliammcclure@gmail.com"><i class="fa fa-send fa-fw"></i> davidwilliammcclure@gmail.com</a></li>
-            <li><a href="https://twitter.com/clured"><i class="fa fa-twitter fa-fw"></i> clured</a></li>
-            <li><a href="https://github.com/davidmcclure"><i class="fa fa-github fa-fw"></i> davidmcclure</a></li>
-        </ul>
-        <ul>
-            <li>Yes to dance beneath the diamond sky.</li>
-            <li>All content licensed under <a href="http://creativecommons.org/licenses/by/3.0/">CC-BY</a>.</li>
-        </ul>
+    <ul>
+
+      <!-- Email. -->
+      <li>
+        <a href="mailto:davidwilliammcclure@gmail.com">
+          <i class="fa fa-send fa-fw"></i> davidwilliammcclure@gmail.com
+        </a>
+      </li>
+
+      <!-- Twitter. -->
+      <li>
+        <a href="https://twitter.com/clured">
+          <i class="fa fa-twitter fa-fw"></i> clured
+        </a>
+      </li>
+
+      <!-- GitHub. -->
+      <li>
+        <a href="https://github.com/davidmcclure">
+          <i class="fa fa-github fa-fw"></i> davidmcclure
+        </a>
+      </li>
+
+    </ul>
+
+    <ul>
+
+      <!-- Poesy pure. -->
+      <li>Yes to dance beneath the diamond sky.</li>
+
+      <!-- License. -->
+      <li>
+        <span>All content licensed under</span>
+        <a href="http://creativecommons.org/licenses/by/3.0">CC-BY</a>.
+      </li>
+
+    </ul>
+
 	</footer><!-- #colophon -->
 
 </div><!-- #page -->
