7be10984c0e9f02bde38298c15f646b92d10e5dd
David McClure <davidwilliammcclure@gmail.com>
2014-08-25 14:59:58 -0400

    Cleans up content-archive.php.

--- content-archive.php
+++ content-archive.php
@@ -1,46 +1,49 @@
 <?php
+
 /**
- * Just show date, title, and metadata.
- *
- * @package Toolbox
- * @since Toolbox 1.0
+ * Post date, title, and metadata.
  */
+
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class('archive'); ?>>
 
 	<header class="entry-header">
-		<?php if ( 'post' == get_post_type() ) : ?>
+		<?php if (get_post_type() == 'post'): ?>
 		<div class="entry-meta">
 			<?php toolbox_posted_on(); ?>
 		</div><!-- .entry-meta -->
 		<?php endif; ?>
-		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'toolbox' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+    <h1 class="entry-title">
+      <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+    </h1>
 	</header><!-- .entry-header -->
 
 	<footer class="entry-meta">
-		<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
+    <?php if (get_post_type() == 'post'): ?>
+
+      <!-- Categories. -->
 			<?php
-				/* translators: used between list items, there is a space after the comma */
-				$categories_list = get_the_category_list( __( ', ', 'toolbox' ) );
-				if ( $categories_list && toolbox_categorized_blog() ) :
+				$categories_list = get_the_category_list(__( ', ', 'toolbox'));
+				if ($categories_list && toolbox_categorized_blog()):
 			?>
 			<span class="cat-links">
-				<?php printf( __( 'Posted to %1$s', 'toolbox' ), $categories_list ); ?>
+				<?php printf(__('Posted to %1$s', 'toolbox'), $categories_list); ?>
 			</span>
 			<span class="sep"> | </span>
-			<?php endif; // End if categories ?>
+			<?php endif; ?>
 
+      <!-- Tags. -->
 			<?php
-				/* translators: used between list items, there is a space after the comma */
-				$tags_list = get_the_tag_list( '', __( ', ', 'toolbox' ) );
+				$tags_list = get_the_tag_list('', __(', ', 'toolbox'));
 				if ( $tags_list ) :
 			?>
 			<span class="tag-links">
-				<?php printf( __( 'Tags: %1$s', 'toolbox' ), $tags_list ); ?>
+				<?php printf(__('Tags: %1$s', 'toolbox'), $tags_list); ?>
 			</span>
-			<?php endif; // End if $tags_list ?>
-		<?php endif; // End if 'post' == get_post_type() ?>
+			<?php endif; ?>
+
+		<?php endif; ?>
 
 	</footer><!-- #entry-meta -->
 
