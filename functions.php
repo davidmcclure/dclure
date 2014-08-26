442edea80401255d4cf8be376aaade87091e39d3
David McClure <davidwilliammcclure@gmail.com>
2014-08-26 12:14:09 -0400

    De-crudifying.

--- functions.php
+++ functions.php
@@ -1,24 +1,6 @@
 <?php
 
 /**
- * Toolbox functions and definitions
- *
- * Sets up the theme and provides some helper functions. Some helper functions
- * are used in the theme as custom template tags. Others are attached to action and
- * filter hooks in WordPress to change core functionality.
- *
- * When using a child theme (see http://codex.wordpress.org/Theme_Development and
- * http://codex.wordpress.org/Child_Themes), you can override certain functions
- * (those wrapped in a function_exists() call) by defining them first in your child theme's
- * functions.php file. The child theme's functions.php file is included before the parent
- * theme's file, so the child theme functions would be used.
- *
- * Functions that are not pluggable (not wrapped in function_exists()) are instead attached
- * to a filter or action hook. The hook can be removed by using remove_action() or
- * remove_filter() and you can attach your own function to the hook.
- *
- * For more information on hooks, actions, and filters, see http://codex.wordpress.org/Plugin_API.
- *
  * @package Toolbox
  * @since Toolbox 0.1
  */
@@ -26,14 +8,7 @@
 
 if (!function_exists('toolbox_setup')):
 /**
- * Sets up theme defaults and registers support for various WordPress features.
- *
- * Note that this function is hooked into the after_setup_theme hook, which runs
- * before the init hook. The init hook is too late for some features, such as indicating
- * support post thumbnails.
- *
- * To override toolbox_setup() in a child theme, add your own toolbox_setup to your child theme's
- * functions.php file.
+ * Register the theme.
  */
 function toolbox_setup() {
 
@@ -64,10 +39,7 @@ add_action('after_setup_theme', 'toolbox_setup');
 
 if (!function_exists('toolbox_posted_on')):
 /**
- * Prints HTML with meta information for the current post-date/time and author.
- * Create your own toolbox_posted_on to override in a child theme
- *
- * @since Toolbox 1.2
+ * Print HTML with meta information for the current date/time and author.
  */
 function toolbox_posted_on() {
 	printf('<time class="entry-date" datetime="%1$s" pubdate>%2$s</time>',
@@ -80,8 +52,6 @@ endif;
 
 /**
  * Returns true if a blog has more than 1 category
- *
- * @since Toolbox 1.2
  */
 function toolbox_categorized_blog() {
 
@@ -106,8 +76,6 @@ function toolbox_categorized_blog() {
 
 /**
  * Flush out the transients used in toolbox_categorized_blog
- *
- * @since Toolbox 1.2
  */
 function toolbox_category_transient_flusher() {
 	delete_transient('category_count');
